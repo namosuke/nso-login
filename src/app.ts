94dd72ca7d250e6e9688acf706963b76e640bf08
ナモすけ <dynamorollerbot@gmail.com>
2022-10-27 03:19:52 +0900

    pull functions

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ class NSOLogin {
     session_token_code_challenge_method: "S256",
   };
 
-  async requestLoginURL() {
+  async pullLoginURL() {
     const url = "https://accounts.nintendo.com/connect/1.0.0/authorize";
     const response = await fetch(
       `${url}?${new URLSearchParams(this.loginParams)}`
@@ -31,7 +31,7 @@ class NSOLogin {
    */
   async getLoginURL() {
     if (this.loginURL === null) {
-      await this.requestLoginURL();
+      await this.pullLoginURL();
     }
     return this.loginURL as string;
   }
@@ -40,7 +40,10 @@ class NSOLogin {
   sessionToken: null | string = null;
   sessionTokenCodeVerifier = "OwaTAOolhambwvY3RXSD-efxqdBEVNnQkc0bBJ7zaak";
 
-  async requestSessionToken(approvalLink: string) {
+  /**
+   * @param approvalLink 「この人にする」のアドレス
+   */
+  async pullSessionToken(approvalLink: string) {
     const sessionTokenCode = /de=(.*?)&/.exec(approvalLink)?.[1];
     if (sessionTokenCode === undefined) {
       throw new Error("Invalid approvalLink");
@@ -75,7 +78,7 @@ class NSOLogin {
    */
   async getSessionToken(approvalLink: string) {
     if (this.sessionToken === null) {
-      await this.requestSessionToken(approvalLink);
+      await this.pullSessionToken(approvalLink);
     }
     return this.sessionToken as string;
   }
@@ -83,16 +86,25 @@ class NSOLogin {
   /** NSOアプリバージョン */
   nsoAppVersion: null | string = null;
 
+  async pullNSOAppVersion() {
+    const url = "https://itunes.apple.com/lookup?id=1234806557";
+    const response = await fetch(url);
+    const json = await response.json();
+    const nsoAppVersion = json.results[0].version;
+    if (typeof nsoAppVersion !== "string") {
+      throw new Error("Cannot find NSO app version in response");
+    }
+    this.nsoAppVersion = nsoAppVersion;
+  }
+
   /**
    * @return NSOアプリバージョン
    */
   async getNSOAppVersion() {
     if (this.nsoAppVersion === null) {
-      const url = "https://itunes.apple.com/lookup?id=1234806557";
-      this.nsoAppVersion = (await (await fetch(url)).json()).results[0]
-        .version as string;
+      await this.pullNSOAppVersion();
     }
-    return this.nsoAppVersion;
+    return this.nsoAppVersion as string;
   }
 
   /** アクセストークン */
@@ -103,39 +115,46 @@ class NSOLogin {
 
   /**
    * @param sessionToken セッショントークン
+   */
+  async pullAccessToken(sessionToken: string) {
+    const url = "https://accounts.nintendo.com/connect/1.0.0/api/token";
+    const params = {
+      client_id: this.loginParams.client_id,
+      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer-session-token",
+      session_token: sessionToken,
+    };
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(params),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status !== 200) {
+      throw new Error("Failed to get access token");
+    }
+    const json = await response.json();
+    const accessToken = json.access_token;
+    if (typeof accessToken !== "string") {
+      throw new Error("Cannot find access_token in response");
+    }
+    this.accessToken = accessToken;
+    const idToken = json.id_token;
+    if (typeof idToken !== "string") {
+      throw new Error("Cannot find id_token in response");
+    }
+    this.idToken = idToken;
+  }
+
+  /**
+   * @param sessionToken セッショントークン
    * @return アクセストークン
    */
   async getAccessToken(sessionToken: string) {
     if (this.accessToken === null) {
-      const url = "https://accounts.nintendo.com/connect/1.0.0/api/token";
-      const params = {
-        client_id: this.loginParams.client_id,
-        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer-session-token",
-        session_token: sessionToken,
-      };
-      const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(params),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (response.status !== 200) {
-        throw new Error("Failed to get access token");
-      }
-      const json = await response.json();
-      const accessToken = json.access_token;
-      if (typeof accessToken !== "string") {
-        throw new Error("Cannot find access_token in response");
-      }
-      this.accessToken = accessToken;
-      const idToken = json.id_token;
-      if (typeof idToken !== "string") {
-        throw new Error("Cannot find id_token in response");
-      }
-      this.idToken = idToken;
+      await this.pullAccessToken(sessionToken);
     }
-    return this.accessToken;
+    return this.accessToken as string;
   }
 
   /**
@@ -143,7 +162,7 @@ class NSOLogin {
    * @param hashMethod NSO is 1, APP is 2
    * @return f APIのリザルト
    */
-  private async getFData(accessToken: string, hashMethod: 1 | 2) {
+  async getFData(accessToken: string, hashMethod: 1 | 2) {
     const url = "https://api.imink.app/f";
     const params = {
       hash_method: hashMethod,
@@ -197,6 +216,48 @@ class NSOLogin {
    * @param idToken IDトークン
    * @param fData f APIのリザルト(NSO)
    * @param nsoAppVersion NSOアプリバージョン
+   */
+  async pullRegistrationToken(
+    idToken: string,
+    fData: FData,
+    nsoAppVersion: string
+  ) {
+    const url = "https://api-lp1.znc.srv.nintendo.net/v3/Account/Login";
+    const params = {
+      parameter: {
+        naCountry: "JP",
+        naBirthday: "2000-01-01",
+        language: "ja-JP",
+        f: fData.f,
+        timestamp: fData.timestamp,
+        requestId: fData.request_id,
+        naIdToken: idToken,
+      },
+    };
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(params),
+      headers: {
+        "Content-Type": "application/json",
+        "x-productversion": nsoAppVersion,
+        "x-Platform": "iOS",
+      },
+    });
+    if (response.status !== 200) {
+      throw new Error("Failed to get registration token");
+    }
+    const json = await response.json();
+    const registrationToken = json.result.webApiServerCredential.accessToken;
+    if (typeof registrationToken !== "string") {
+      throw new Error("Cannot find webApiServerCredential in response");
+    }
+    this.registrationToken = registrationToken;
+  }
+
+  /**
+   * @param idToken IDトークン
+   * @param fData f APIのリザルト(NSO)
+   * @param nsoAppVersion NSOアプリバージョン
    * @return 登録トークン
    */
   async getRegistrationToken(
@@ -205,38 +266,9 @@ class NSOLogin {
     nsoAppVersion: string
   ) {
     if (this.registrationToken === null) {
-      const url = "https://api-lp1.znc.srv.nintendo.net/v3/Account/Login";
-      const params = {
-        parameter: {
-          naCountry: "JP",
-          naBirthday: "2000-01-01",
-          language: "ja-JP",
-          f: fData.f,
-          timestamp: fData.timestamp,
-          requestId: fData.request_id,
-          naIdToken: idToken,
-        },
-      };
-      const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(params),
-        headers: {
-          "Content-Type": "application/json",
-          "x-productversion": nsoAppVersion,
-          "x-Platform": "iOS",
-        },
-      });
-      if (response.status !== 200) {
-        throw new Error("Failed to get registration token");
-      }
-      const json = await response.json();
-      const registrationToken = json.result.webApiServerCredential.accessToken;
-      if (typeof registrationToken !== "string") {
-        throw new Error("Cannot find webApiServerCredential in response");
-      }
-      this.registrationToken = registrationToken;
+      await this.pullRegistrationToken(idToken, fData, nsoAppVersion);
     }
-    return this.registrationToken;
+    return this.registrationToken as string;
   }
 
   /** f API(App)のリザルト */
@@ -260,6 +292,48 @@ class NSOLogin {
    * @param registrationToken 登録トークン
    * @param fDataApp f APIのリザルト(App)
    * @param nsoAppVersion NSOアプリバージョン
+   */
+  async pullWebServiceToken(
+    registrationToken: string,
+    fDataApp: FData,
+    nsoAppVersion: string
+  ) {
+    const url =
+      "https://api-lp1.znc.srv.nintendo.net/v2/Game/GetWebServiceToken";
+    const params = {
+      parameter: {
+        f: fDataApp.f,
+        timestamp: fDataApp.timestamp,
+        requestId: fDataApp.request_id,
+        registrationToken: registrationToken,
+        id: 4834290508791808,
+      },
+    };
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(params),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${registrationToken}`,
+        "x-productversion": nsoAppVersion,
+        "x-Platform": "iOS",
+      },
+    });
+    if (response.status !== 200) {
+      throw new Error("Failed to get web service token");
+    }
+    const json = await response.json();
+    const webServiceToken = json.result.accessToken;
+    if (typeof webServiceToken !== "string") {
+      throw new Error("Cannot find webServiceToken in response");
+    }
+    this.webServiceToken = webServiceToken;
+  }
+
+  /**
+   * @param registrationToken 登録トークン
+   * @param fDataApp f APIのリザルト(App)
+   * @param nsoAppVersion NSOアプリバージョン
    * @return ウェブサービストークン
    */
   async getWebServiceToken(
@@ -268,38 +342,13 @@ class NSOLogin {
     nsoAppVersion: string
   ) {
     if (this.webServiceToken === null) {
-      const url =
-        "https://api-lp1.znc.srv.nintendo.net/v2/Game/GetWebServiceToken";
-      const params = {
-        parameter: {
-          f: fDataApp.f,
-          timestamp: fDataApp.timestamp,
-          requestId: fDataApp.request_id,
-          registrationToken: registrationToken,
-          id: 4834290508791808,
-        },
-      };
-      const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(params),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${registrationToken}`,
-          "x-productversion": nsoAppVersion,
-          "x-Platform": "iOS",
-        },
-      });
-      if (response.status !== 200) {
-        throw new Error("Failed to get web service token");
-      }
-      const json = await response.json();
-      const webServiceToken = json.result.accessToken;
-      if (typeof webServiceToken !== "string") {
-        throw new Error("Cannot find webServiceToken in response");
-      }
-      this.webServiceToken = webServiceToken;
+      await this.pullWebServiceToken(
+        registrationToken,
+        fDataApp,
+        nsoAppVersion
+      );
     }
-    return this.webServiceToken;
+    return this.webServiceToken as string;
   }
 
   /** バレットトークン */
@@ -307,29 +356,36 @@ class NSOLogin {
 
   /**
    * @param webServiceToken ウェブサービストークン
+   */
+  async pullBulletToken(webServiceToken: string) {
+    const url = "https://api.lp1.av5ja.srv.nintendo.net/api/bullet_tokens";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "x-web-view-ver": "1.0.0-5644e7a2",
+        Cookie: `_gtoken=${webServiceToken}`,
+      },
+    });
+    if (response.status !== 201) {
+      throw new Error("Failed to get bullet token");
+    }
+    const json = await response.json();
+    const bulletToken = json.bulletToken;
+    if (typeof bulletToken !== "string") {
+      throw new Error("Cannot find bulletToken in response");
+    }
+    this.bulletToken = bulletToken;
+  }
+
+  /**
+   * @param webServiceToken ウェブサービストークン
    * @return バレットトークン
    */
   async getBulletToken(webServiceToken: string) {
     if (this.bulletToken === null) {
-      const url = "https://api.lp1.av5ja.srv.nintendo.net/api/bullet_tokens";
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "x-web-view-ver": "1.0.0-5644e7a2",
-          Cookie: `_gtoken=${webServiceToken}`,
-        },
-      });
-      if (response.status !== 201) {
-        throw new Error("Failed to get bullet token");
-      }
-      const json = await response.json();
-      const bulletToken = json.bulletToken;
-      if (typeof bulletToken !== "string") {
-        throw new Error("Cannot find bulletToken in response");
-      }
-      this.bulletToken = bulletToken;
+      await this.pullBulletToken(webServiceToken);
     }
-    return this.bulletToken;
+    return this.bulletToken as string;
   }
 
   /**
@@ -345,7 +401,7 @@ const nsoLogin = new NSOLogin();
   const loginURL = await nsoLogin.getLoginURL();
   console.log(loginURL);
   const approvalLink =
-    "npf71b963c1b7b6d119://auth#session_state=da0d403b20ed1128cba9c3da46386e049587bccbad582199ca225b8b692e59b9&session_token_code=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLm5pbnRlbmRvLmNvbSIsImlhdCI6MTY2NjgwNjkxMiwic3RjOm0iOiJTMjU2IiwidHlwIjoic2Vzc2lvbl90b2tlbl9jb2RlIiwiYXVkIjoiNzFiOTYzYzFiN2I2ZDExOSIsInN1YiI6IjQ5N2NiZTJmZGI3YjA3OWIiLCJleHAiOjE2NjY4MDc1MTIsInN0YzpjIjoidFlMUE81UHhwSy1EVGNBSEpYdWdEN3p0dkFaUWxvMERRUXAzYXU1enR1TSIsImp0aSI6IjYzNzU2NjE3NTM3Iiwic3RjOnNjcCI6WzAsOCw5LDE3LDIzXX0.kdYseoTA2mxMbaLNuCoKr_n8EMXAcpzh3dBHZMRLK2I&state=V6DSwHXbqC4rspCn_ArvfkpG1WFSvtNYrhugtfqOHsF6SYyX";
+    "npf71b963c1b7b6d119://auth#session_state=da0d403b20ed1128cba9c3da46386e049587bccbad582199ca225b8b692e59b9&session_token_code=eyJhbGciOiJIUzI1NiJ9.eyJzdGM6YyI6InRZTFBPNVB4cEstRFRjQUhKWHVnRDd6dHZBWlFsbzBEUVFwM2F1NXp0dU0iLCJqdGkiOiI2Mzc1NzI5NzcxOCIsInR5cCI6InNlc3Npb25fdG9rZW5fY29kZSIsInN0YzpzY3AiOlswLDgsOSwxNywyM10sInN0YzptIjoiUzI1NiIsImV4cCI6MTY2NjgwODkzNiwiaWF0IjoxNjY2ODA4MzM2LCJzdWIiOiI0OTdjYmUyZmRiN2IwNzliIiwiYXVkIjoiNzFiOTYzYzFiN2I2ZDExOSIsImlzcyI6Imh0dHBzOi8vYWNjb3VudHMubmludGVuZG8uY29tIn0.OxXFds1woLO0dOhlhMR6BpfWdCXKGkJS9UIREs6j6Ek&state=V6DSwHXbqC4rspCn_ArvfkpG1WFSvtNYrhugtfqOHsF6SYyX";
   const sessionToken = await nsoLogin.getSessionToken(approvalLink);
   console.log(sessionToken);
   const nsoAppVersion = await nsoLogin.getNSOAppVersion();
